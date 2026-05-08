634763a65ab0605677cb2363278dece8592bfd6d
Sam Switz <sds9bt@gmail.com>
2026-05-07 23:50:54 -0500

    aggregated binomial model

--- 05_model_eval/model_eval.R
+++ 05_model_eval/model_eval.R
@@ -4,8 +4,15 @@ library(coda)
 
 x <- readRDS("04_model_fit/samples.rds")
 
-plot(x[, c("beta", "sigma_sq_bank", "sigma_sq_state", "sigma_sq_sector")])
+plot(x[, c("beta", "sigma_sq_bank", "sigma_sq_state", "sigma_sq_sector")], ask = TRUE)
 
 summary(x)
 
-dic.samples(model, 1000, type="pD")
+## Eval 1 DIC values
+
+dic.samples(model, 10000, type="pD")
+
+
+## Eval 2: Chi-Square discrepancy
+
+# generate y tilde for a given theta by simulating from the data model (replicate data set)
